c356ed6495b1090f079604a02b99df3d2a0ae530
Husanbek <buxorojahon@gmail.com>
2025-07-10 20:00:31 +0500

    Fetch

--- admin/src/middlewares/Fetch.tsx
+++ admin/src/middlewares/Fetch.tsx
@@ -3,8 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const Fetch = axios.create({
-  // baseURL: "https://crm-biohazard.onrender.com/api/",
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://crm-biohazard.onrender.com/api/",
   headers: {
     Authorization: token,
   },
