f1a8cfb1e802f1e3c8cbbdc37f6c9d7feded1706
Husanbek Zayniddinov <147181317+biohazardcoder@users.noreply.github.com>
2025-07-16 06:02:50 +0500

    Update Fetch.tsx

--- admin/src/middlewares/Fetch.tsx
+++ admin/src/middlewares/Fetch.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const Fetch = axios.create({
-  baseURL: "http://srv.mukammal-crm.uz/api/",
+  baseURL: "https://srv.mukammal-crm.uz/api/",
   // baseURL: "http://localhost:4000/api/",
   headers: {
     Authorization: token,
